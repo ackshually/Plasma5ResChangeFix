bc7c39bd38d0960c391b30d30d861f73c7666f2e
ackshually <77393110+ackshually@users.noreply.github.com>
2022-05-02 23:27:36 +0200

    Update resfix_save.sh

--- resfix_save.sh
+++ resfix_save.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Saving windows position and size as comma separated values
 wins=$(xdotool search --name ".+")
 for win in $wins; do
 	eval $(xdotool getwindowgeometry --shell $win)
@@ -10,12 +11,14 @@ for win in $wins; do
 	heights=$heights","$HEIGHT
 done
 
+# Removing the 1st comma
 heights="heights=${heights:1}"
 widths="widths=${widths:1}"
 winids="winids=${winids:1}"
 xs="xs=${xs:1}"
 ys="ys=${ys:1}"
 
+# Saving windows data to a file
 cd ~/.config
 echo $heights > resFixData
 echo $widths >> resFixData
@@ -23,6 +26,7 @@ echo $winids >> resFixData
 echo $xs >> resFixData
 echo $ys >> resFixData
 
+# Saving icon locations using "plasma desktop scripting"
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
 var desk = desktops()[0];
 var key = "positions";
