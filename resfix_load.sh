dba45433b9b7833ddc592777c578ca38a633e80a
ackshually <77393110+ackshually@users.noreply.github.com>
2022-05-02 23:18:23 +0200

    Update resfix_load.sh

--- resfix_load.sh
+++ resfix_load.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-cp ~/.config/resConfig ~/.config/resConfigTmp
+# Making copy of 
+cp ~/.config/resFixData ~/.config/resFixData.tmp
 
 xrandr -s 1920x1080
 sleep 2
 
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
 var desk = desktops()[0];
-var cf = ConfigFile("resConfigTmp");
+var cf = ConfigFile("resFixData.tmp");
 var oldConf = cf.readEntry("positions");
 desk.currentConfigGroup = "General";
 desk.writeConfig("positions", oldConf.split(","));
 cf.deleteEntry("positions");'
 
-. ~/.config/resConfigTmp
+# Loading window data and splitting it to arrays
+. ~/.config/resFixData.tmp
 oldIFS=$IFS
 IFS=','
 heights=($heights)
@@ -23,10 +25,11 @@ xs=($xs)
 ys=($ys)
 IFS=$oldIFS
 
+# Moving and resizing windows
 for (( n=0; n<${#winids[@]}; n++ ))
 do
 	xdotool windowmove ${winids[n]} ${xs[n]} ${ys[n]}
 	xdotool windowsize ${winids[n]} ${widths[n]} ${heights[n]}
 done
 
-rm ~/.config/resConfigTmp
+rm ~/.config/resFixData.tmp
